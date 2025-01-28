4efc4138af469718aa0a8a1e3d1bb7d2fe6cea9c
Specter242 <aaroneast@tlacil.com>
2025-01-28 15:38:37 -0500

    test tesxtnode

---
+++ src/test_textnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from textnode import TextNode, TextType
+
+
+class TestTextNode(unittest.TestCase):
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Bold_text)
+        node2 = TextNode("This is a text node", TextType.Bold_text)
+        self.assertEqual(node, node2)
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Italic_text)
+        node2 = TextNode("This is a text node", TextType.Bold_text)
+        self.assertNotEqual(node, node2)
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Code_text)
+        node2 = TextNode("This is a text node", TextType.Bold_text)
+        self.assertNotEqual(node, node2)
+    def test_eq(self):
+        node = TextNode("This is a text node", TextType.Link)
+        node2 = TextNode("This is a text node", TextType.Bold_text)
+        self.assertNotEqual(node, node2)
+
+
+if __name__ == "__main__":
+    unittest.main()
